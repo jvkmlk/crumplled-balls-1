72e635e2d2336989d6aad61dfaa620f6d013b8e4
jvkmlk <72926365+jvkmlk@users.noreply.github.com>
2020-12-11 10:21:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,13 +10,13 @@ var paper;
 var ground;
 
 function setup() {
-  createCanvas(1850, 700);
+  createCanvas(800, 700);
   engine = Engine.create();
 	world = engine.world;
   rectMode(CENTER);
-  dustbin1 = new Dustbin(1700,685,160,20., {isStatic:true} );
-	dustbin2= new Dustbin(1780,680,20,300);
-	dustbin3= new Dustbin(1630,680,20,300);
+  dustbin1 = new Dustbin(700,685,160,20., {isStatic:true} );
+	dustbin2= new Dustbin(770,680,20,300);
+	dustbin3= new Dustbin(620,680,20,300);
 
 
  paper=new Paper (100,300,10);
@@ -38,14 +38,16 @@ function draw() {
   dustbin2.display();
   dustbin3.display();
   paper.display();
+  textSize(50);
+  text ("Press Space to serve",100,50);
 
   
   drawSprites();
  
 }
 function keyPressed()  {
-  if (keyCode === UP_ARROW){
-Body.applyForce(paper.body,paper.body.position,{x:10,y:-10});
+  if (keyCode ===32){
+Body.applyForce(paper.body,paper.body.position,{x:10,y:-20});
   }
 }
 
